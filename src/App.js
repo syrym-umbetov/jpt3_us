1235128f8dcbee2078ecc9446d10b5ea6ba9e1e7
syrym-umbetov <umbetov.syrym@gmail.com>
2021-11-17 10:02:31 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import { Footer, Blog, Possibility, Features, WhatGPTS, Header } from './containers'
+import { CTA, Brand, Navbar } from './components'
+
+const App = () => {
+    return (
+        <div>
+            <h1>GPT-3</h1>
+            
+        </div>
+    )
+}
+
+export default App
