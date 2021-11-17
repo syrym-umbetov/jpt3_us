af88ec15ff0ee96aa87539120a1427a71b627e26
syrym-umbetov <umbetov.syrym@gmail.com>
2021-11-17 10:20:09 +0600

    second commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from 'react'
 
-import { Footer, Blog, Possibility, Features, WhatGPTS, Header } from './containers'
+import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from './containers'
 import { CTA, Brand, Navbar } from './components'
 
 const App = () => {
     return (
-        <div>
-            <h1>GPT-3</h1>
-            
+        <div className="App">
+            <div className="gradient_bg">
+                <Navbar />
+                <Header />
+            </div>
+            <Brand />
+            <WhatGPT3 />
+            <Features />
+            <Possibility />
+            <CTA />
+            <Blog />
+            <Footer />
         </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
-//import ReactDOM from 'react-dom';
-
-//import App from './App';
-
 //ReactDOM.render(<App />, document.getElementByld('root'))
+
+import ReactDOM from 'react-dom';
+import App from './App';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
